c706147eeea5149dd4e2078856b51afd5df47a91
vitor184 <vitor.moreira.silva02@escola.pr.gov.br>
2025-06-05 19:18:25 -0300

    aula 05/06 1

---
+++ perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'Programação', 
+    'O que é Python?', 
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao (
+    'Programação',
+    'O que é JavaScript?',
+    'JavaScript é uma linguagem de programação'
+)
+criaCartao (
+    'lingua inglesa',
+    'como se diz OI em ingles?'
+    'Oi em ingles é Hi (rai)'
+)
